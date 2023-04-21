4e1ddb3e280c0b006862acbe98c6966e6f05bbf7
Stefano Giovanoni <stefanogiovannonii@gmail.com>
2023-04-20 23:32:04 -0300

    Checking if comment shows up

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,11 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Making sure this shows up in GitHub
         Console.Write("What is your first name? ");
         string name = Console.ReadLine();
         Console.Write("What is your last name? ");
         string lastname = Console.ReadLine();
-        
+        Console.WriteLine("");
         Console.WriteLine($"Your name is {lastname}, {name} {lastname}.");
     }
 }
