965bcfc6e544d4ca6f3be33c2562b5c413f38dba
Stefano Giovanoni <stefanogiovannonii@gmail.com>
2023-04-21 01:04:34 -0300

    C# Prep 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+
+        string letter = "";
+        if ( grade >= 90)
+        { letter = "A";}
+        else if ( grade >= 80)
+        { letter = "B";}
+        else if ( grade >= 70)
+        { letter = "C";}
+        else if ( grade >= 60)
+        { letter = "D";}
+        else
+        {letter = "F";}
+
+        Console.WriteLine($"Your grade is: {letter} ");
+
+        if ( grade >= 70 )
+        {Console.Write("Congratulations! You have passed. ");}
+        else
+        {Console.Write("You have not passed. You can do it next semester! ");}
+
+
+
+
+
+
     }
 }
