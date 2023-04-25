08be09f0aeae45e40d295fe86b908c87b8e08590
Stefano Giovanoni <stefanogiovannonii@gmail.com>
2023-04-25 00:39:21 -0300

    prep 03

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess =-1;
+        
+        
+        while(guess != number){
+        
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+
+            if ( guess > number)
+            { Console.Write("Lower"); }
+            else if ( guess < number)
+            { Console.Write("Higher");}
+            else {
+                Console.Write("You guessed it!");
+            }
+
+
+        }
+        
+        
+        
+        
     }
+    
 }
