d18b312f93e840250512f339c5da7fc79ddefa60
Stefano Giovanoni <stefanogiovannonii@gmail.com>
2023-06-02 12:51:39 -0300

    assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        
+        Assignment first = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(first.GetSummary());
+
+        
+        MathAssignment second = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(second.GetSummary());
+        Console.WriteLine(second.GetHomeworkList());
+
+        WritingAssignment third = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(third.GetSummary());
+        Console.WriteLine(third.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class WritingAssignment : Assignment{
+    private string _title;
+    public WritingAssignment(string title, string studentName, string topic) 
+    :base(studentName, topic){
+        _title = title;
+    }
+
+    public string GetWritingInformation(){
+        return _studentName + _title;
+    }
+}
